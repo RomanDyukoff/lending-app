8e40ec26d49cded5dc3a9d9c1fa0d1de188b726e
DESKTOP-4M5140Q\romad <hlebahleb@gmail.com>
2023-11-14 20:24:53 +0300

    fix: bugs

--- src/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-import { PageLayout } from '@/layout/layout';
-
-export default function NotFoundPage(): JSX.Element {
-	return (
-		<PageLayout title="Ошибка | Такой страницы не существует">
-			<div>Упс...</div>
-		</PageLayout>
-	);
-}
